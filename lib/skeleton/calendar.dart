0757327d5e80a397d3718961d1db3669df6b40ec
ValT78 <118465560+ValT78@users.noreply.github.com>
2024-08-07 17:23:07 +0200

    Avoid doublon in calendar

--- lib/skeleton/calendar.dart
+++ lib/skeleton/calendar.dart
@@ -25,79 +25,79 @@ class CalendarState extends State<Calendar> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: (equipPicked.equipList.length + 1) * (months.length + 2), // +1 pour la ligne des labels, +2 pour les colonnes d'équipement et 'Total'
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: months.length + 2, // +2 pour les colonnes d'équipement et 'Total'
-        childAspectRatio: 1, // Vous pouvez ajuster cet aspect ratio en fonction de vos besoins
-      ),
-      itemBuilder: (context, index) {
-        final row = index ~/ (months.length + 2);
-        final column = index % (months.length + 2);
+  final equipNames = selectedCalendar.keys.toList();
 
-        if (row == 0) {
-          // C'est la ligne des labels
-          if (column == 0) {
-            return const GridTile(child: Center(child: Text('Equipement')));
-          } else if (column == months.length + 1) {
-            return const GridTile(child: Center(child: Text('Total')));
-          } else {
-            // Ajout d'une teinte de bleu aux labels des mois
-            return GridTile(
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: () {},
-                  highlightColor: Colors.transparent, // Suppression de la couleur de sélection
-                  child: Container(
-                    margin: const EdgeInsets.all(2),
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 200 - column * 10, 200 - column * 10, 255), // Application de la couleur de fond à la case
-                    ),
-                    child: Center(
-                      child: Text(months[column - 1]),
-                    ),
-                  ),
-                ),
-              ),
-            );
-          }
+  return GridView.builder(
+    itemCount: (selectedCalendar.length + 1) * (months.length + 2), // +1 pour la ligne des labels, +2 pour les colonnes d'équipement et 'Total'
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: months.length + 2, // +2 pour les colonnes d'équipement et 'Total'
+      childAspectRatio: 1, // Vous pouvez ajuster cet aspect ratio en fonction de vos besoins
+    ),
+    itemBuilder: (context, index) {
+      final row = index ~/ (months.length + 2);
+      final column = index % (months.length + 2);
+
+      if (row == 0) {
+        // C'est la ligne des labels
+        if (column == 0) {
+          return const GridTile(child: Center(child: Text('Equipement')));
+        } else if (column == months.length + 1) {
+          return const GridTile(child: Center(child: Text('Total')));
         } else {
-          // C'est une des lignes d'équipement
-          final equip = equipPicked.equipList[row - 1]; // -1 pour compenser la ligne des labels
-
-          if (column == 0) {
-            // C'est la colonne d'équipement
-            return GridTile(child: Center(child: Text(equip.equipName)));
-          } else if (column == months.length + 1) {
-            // C'est la colonne 'Total'
-            var total = selectedCalendar[equip.equipName]?.values.where((e) => e).length;
-            return GridTile(child: Center(child: Text('$total')));
-          } else {
-            // C'est une des colonnes de mois
-            final month = months[column - 1];
-            return GridTile(
-              child: Material( // Ajout du widget Material
-                color: Colors.transparent, // Pour rendre le Material transparent
-                child: InkWell(
-                  onTap: () {
-                    setState(() {
-                      selectedCalendar[equip.equipName]![month] = !selectedCalendar[equip.equipName]![month]!;
-                    });
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.all(2),
-                    // decoration: BoxDecoration(
-                    //   color: selectedCalendar[equip.equipName]![month]! ? Colors.grey : Colors.grey[200],
-                    // ),
-                    child: selectedCalendar[equip.equipName]![month]! ? const Center(child: Text('X')) : null,
+          // Ajout d'une teinte de bleu aux labels des mois
+          return GridTile(
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {},
+                highlightColor: Colors.transparent, // Suppression de la couleur de sélection
+                child: Container(
+                  margin: const EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 200 - column * 10, 200 - column * 10, 255), // Application de la couleur de fond à la case
+                  ),
+                  child: Center(
+                    child: Text(months[column - 1]),
                   ),
                 ),
               ),
-            );
-          }
+            ),
+          );
         }
-      },
-    );
-  }
+      } else {
+        // C'est une des lignes d'équipement
+        final equipName = equipNames[row - 1]; // -1 pour compenser la ligne des labels
+
+        if (column == 0) {
+          // C'est la colonne d'équipement
+          return GridTile(child: Center(child: Text(equipName)));
+        } else if (column == months.length + 1) {
+          // C'est la colonne 'Total'
+          var total = selectedCalendar[equipName]?.values.where((e) => e).length;
+          return GridTile(child: Center(child: Text('$total')));
+        } else {
+          // C'est une des colonnes de mois
+          final month = months[column - 1];
+          return GridTile(
+            child: Material( // Ajout du widget Material
+              color: Colors.transparent, // Pour rendre le Material transparent
+              child: InkWell(
+                onTap: () {
+                  setState(() {
+                    selectedCalendar[equipName]![month] = !selectedCalendar[equipName]![month]!;
+                  });
+                },
+                child: Container(
+                  margin: const EdgeInsets.all(2),
+                  child: selectedCalendar[equipName]![month]! ? const Center(child: Text('X')) : null,
+                ),
+              ),
+            ),
+          );
+        }
+      }
+    },
+  );
+}
+
 }
